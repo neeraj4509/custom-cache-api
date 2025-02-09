8f20cdddad3066e4e39a26cd4f6c4c4f64508285
Neeraj Bendkhale <87565643+neeraj4509@users.noreply.github.com>
2025-02-09 13:23:15 +0530

    init commit

--- src/main/java/com/example/customcache/CustomCacheApiApplication.java
+++ src/main/java/com/example/customcache/CustomCacheApiApplication.java
@@ -10,7 +10,7 @@ import java.net.InetAddress;
 public class CustomCacheApiApplication {
     public static void main(String[] args) throws Exception {
         Environment env = SpringApplication.run(CustomCacheApiApplication.class, args).getEnvironment();
-        String port = env.getProperty("server.port", "8080"); // Default to 8080 if not set
+        String port = env.getProperty("server.port", "8080"); 
         String host = InetAddress.getLocalHost().getHostAddress();
 
         System.out.println("Custom Cache API is running on http://" + host + ":" + port);
